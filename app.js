7df2af6ddb19c5907a75513f17673899764a920d
Ibodulla <bobir_1997@mail.ru>
2020-02-14 10:57:59 +0400

    Initial files

---
+++ app.js
@@ -0,0 +1,21 @@
+
+
+let maxSlices = 17;
+let pizzaTypeCount = 4;
+
+let pizzaTypes = [2, 5, 6, 8];
+
+
+
+/**
+ * 1. pizzaTypeCount should be equal to pizzaTypes.length
+ * 2. maxSlices should not exceed pizzaTypes overall sum
+ * 
+ * 
+*/
+
+// 
+
+const calculateOrderNumber = (maxSlices, pizzaTypeCount, pizzaTypes) => {
+    
+}
